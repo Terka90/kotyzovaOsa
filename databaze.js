bd995600ef4c1ced015e06a43e95ed5ab45aaac8
Tereza Kotyzova <dzesspi@gmail.com>
2022-01-30 12:29:48 +0100

    Úprava souborů - menší reorganizace kódu a html stránek

--- databaze.js
+++ databaze.js
@@ -1,53 +1,63 @@
-class IndexedDB{
-    constructor(){
-        this._db = null;
-        this._verze = 1;
-        this.pripoj();
+
+    const newUser = {username:"John", firstname:"Terka", lastname:"New York", age:30};
+
+
+function registruj(){
+//userName; fName; lName; age
+
+    newUser["username"] = document.getElementById("userName").value;
+    newUser["firstname"] = document.getElementById("fName").value;
+    newUser["lastname"] = document.getElementById("lName").value;
+    newUser["age"] = document.getElementById("age").value;
+
+    console.log(newUser["username"] + "; " + newUser["firstname"] + "; " + newUser["lastname"] + "; " +  newUser["age"]);
+
+
+    //text ve formuláři nastaví na prázdno
+    document.getElementById("userName").value = "";
+    document.getElementById("fName").value = "";
+    document.getElementById("lName").value = "";
+    document.getElementById("age").value = "";
+
+}
+
+function vypisUzivatele(){
+    document.getElementById("vypis").innerHTML = newUser["username"];
+}
+
+
+function overUzivatele(){
+
+
+        //newUser["username"] = document.getElementById("userName").value;
+
+
+
+    //nejdřív zkontroluji, jestli mám v textovém okně nějakou hodnotu
+    if(document.getElementById("userName").value == ""){
+        console.log("Je to prázdný!");
+
+
+    //pokud tam tu hodnotu mám, tak ji porovnám, jestli už náhodou neexistuje
+    }else{
+            var uzivatel = newUser["username"];
+            var novyUzivatel = document.getElementById("userName").value;
+
+            console.log(uzivatel);
+            console.log(novyUzivatel);
+
+            if(uzivatel == novyUzivatel){
+                console.log("Vítej " + newUser["username"] + "!");
+                window.location.href = "prihlaseno.html";
+            }else{
+                console.log("Tuto přezdívku neevidujeme. Vytvořte si registraci. ");
+            }
+
     }
-
-
-
-
-        pripoj(){
-            const request = indexedDB.open('wwwapp2021KS', this._verze);
-            request.onsuccess = (ev) => {
-                ///this._db = ev. target.result;
-                this._db = request.result;
-                console.log('DB', this._db);
-            }
-
-            request.onupgradeneeded = (ev) => {
-                /// vytvareni a aktualizace DB
-                console.log(ev);
-                switch (ev.oldVersion) {
-                    /// zcela nova DB
-                    case 0:
-                        break;
-                    /// upgrade z 1 na novou strukturu
-                    case 1:
-                        /// ev.target.result je reference na DB
-                        const os = ev.target.result.createObjectStore('uzivatele', {keyPath : 'id', autoIncrement: true})
-                        os.createIndex('prijmeniIndex', 'prijmeni');
-                        os.createIndex('jmenoIndex', 'jmeno');
-
-                        os.add({jmeno: 'Tereza', prijmeni: 'Kotyzová'});
-                        break;
-
-                }
-
-
-            }
-
-
-
-
-
-
-
-
-    const DB = new IndexedDB();
-}
 }
 
 
 
+function odhlasit(){
+    window.location.href = "osaobsah.html";
+}
